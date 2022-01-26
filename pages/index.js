60cb464e9d9e0a14810a562d3a5f5cecc0808ae6
Jonathan S. Machado <jonathansmachado@gmail.com>
2022-01-25 22:53:37 -0300

    adicionada a página do chat

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
 import appConfig from "../config.json";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 function Title(props) {
   const Tag = props.tag || "h1";
@@ -49,11 +20,12 @@ function Title(props) {
 }
 
 export default function PaginaInicial() {
-  const username = "JonathanSMachado";
+  // const username = "JonathanSMachado";
+  const [username, setUsername] = useState("JonathanSMachado");
+  const router = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -88,6 +60,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              router.push("/chat");
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -119,6 +95,7 @@ export default function PaginaInicial() {
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
               }}
+              onChange={(e) => setUsername(e.target.value)}
             />
             <Button
               type="submit"
